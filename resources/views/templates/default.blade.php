c98bd3fe9eefb85cea0d7364272a23de58a0e7cd
denys.p <denys.p@smart-gamma.com>
2020-09-18 17:49:06 +0300

    fix jquery

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -14,6 +14,7 @@
         @yield('styles')
         <link rel="stylesheet" href="{{ URL::asset('/css/style.css') }}">
         <title>Heals</title>
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
     </head>
 
     <body>
